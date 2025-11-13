b05f0b5959f7c73bd7d1ccb0636f423ff6186cc1
Alex Spock <awspock@gmail.com>
2025-11-13 08:40:10 -0700

    updated common.js to handle dates correctly after changing database to UTC

--- js/common.js
+++ js/common.js
@@ -8,31 +8,12 @@ async function ready(fn) {
 
 var loader = new Loader("Loader");
 
-function easternToLocal(easternDateString) {
-  const eastern = "America/New_York";
-  const localTZ = Intl.DateTimeFormat().resolvedOptions().timeZone;
-
-  // Interpret as Eastern Time
-  const easternDate = new Date(
-    new Date(easternDateString).toLocaleString("en-US", {
-      timeZone: eastern,
-    })
-  );
-
-  // Convert to local time
-  return new Date(
-    easternDate.toLocaleString("en-US", {
-      timeZone: localTZ,
-    })
-  );
-}
-
 function convertDateFields() {
   var datesToConvert = document.querySelectorAll("[data-dateformatter]");
 
   datesToConvert.forEach(function (el) {
     var odate = el.textContent;
-    el.textContent = easternToLocal(odate).toLocaleString("en-US");
+    el.textContent = new Date(odate + " UTC").toLocaleString("en-US");
     el.removeAttribute("data-dateformatter");
   });
 }
